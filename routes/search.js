92f2b86009c30625bf7fa16ffd72432185935556
Daniel Granerud <daniel.granerud@payex.com>
2020-08-18 13:02:24 +0200

    Use express-async-handler

--- routes/search.js
+++ routes/search.js
@@ -2,26 +2,21 @@ var express = require('express');
 var router = express.Router();
 const { Client } = require('@elastic/elasticsearch')
 const client = new Client({ node: 'http://localhost:9200' })
+const asyncHandler = require('express-async-handler')
 
-router.get('/', async (req, res, next) => {
+router.get('/', asyncHandler(async (req, res, next) => {
   var return_message = "error"
-  try {
-    const { body } = await client.search({
-      index: 'developer-*',
-      // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
-      body: {
-        query: {
-          match: { quote: 'reversal' }
-        }
+  const { body } = await client.search({
+    index: 'developer-*',
+    // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+    body: {
+      query: {
+        match: { quote: 'reversal' }
       }
-    })
-
-    return next(error);
-  } catch (error) {
-    return_message = JSON.stringify(error);
-  }
+    }
+  })
 
   res.send(return_message)
-});
+}));
 
 module.exports = router;
