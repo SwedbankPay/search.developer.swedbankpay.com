bd5a303f46a52b7cd2e82a15919baba7f71e652f
Daniel Granerud <daniel.granerud@payex.com>
2020-08-19 14:10:38 +0200

    Added nodemon and have a sucessfull search

--- routes/search.js
+++ routes/search.js
@@ -4,19 +4,21 @@ const { Client } = require('@elastic/elasticsearch')
 const client = new Client({ node: 'http://localhost:9200' })
 const asyncHandler = require('express-async-handler')
 
-router.get('/', asyncHandler(async (req, res, next) => {
-  var return_message = "error"
+router.get('/:?', asyncHandler(async (req, res, next) => {
+  var query = req.params.query;
+  if (query == null || query.length == 0) {
+    query = "developer portal"
+  }
   const { body } = await client.search({
     index: 'developer-*',
-    // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
     body: {
       query: {
-        match: { quote: 'reversal' }
+        match_phrase: { quote: query }
       }
     }
   })
 
-  res.send(return_message)
+  res.send(body)
 }));
 
 module.exports = router;
