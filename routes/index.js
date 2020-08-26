13ed00464106523cc90aad5933f032d5a7efe9ce
Daniel Granerud <daniel.granerud@payex.com>
2020-08-26 07:44:32 +0200

    Updated title

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = Express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
-    title: 'Express'
+    title: 'Simple elastic search client'
   });
 });
 
