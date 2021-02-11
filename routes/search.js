3a13c348979e24e07e7d6d9c0cdc1ce53fd547b7
Daniel Granerud <daniel.granerud@payex.com>
2021-02-11 10:15:19 +0100

    Update how we do pagination

--- routes/search.js
+++ routes/search.js
@@ -8,69 +8,73 @@ const elasticPassword = process.env.elasticPassword || 'none'
 const elasticIndex = process.env.elasticIndex || 'devportal'
 
 const client = new Client.Client({
-  node: elasticUrl,
-  auth: {
-    username: elasticUsername,
-    password: elasticPassword
-  },
+    node: elasticUrl,
+    auth: {
+        username: elasticUsername,
+        password: elasticPassword
+    },
 })
 var asyncHandler = require('express-async-handler');
 
 
-router.get('/', asyncHandler(async (req, res, next) => {
-  var query = req.query.q + ''; //Force into a string
-  if (query == null || query.length == 0) {
-    query = "developer portal"
-  }
-  var startIndex = req.query.page;
-  if (startIndex == null)
-    startIndex = 0;
-  var querySize = req.query.size;
-  if (querySize == null)
-    querySize = 10;
+router.get('/', asyncHandler(async(req, res, next) => {
+    var query = req.query.q + ''; //Force into a string
+    if (query == null || query.length == 0) {
+        query = "developer portal"
+    }
+    var startIndex = req.query.page;
+    if (startIndex == null)
+        startIndex = 0;
+    var querySize = req.query.size;
+    if (querySize == null)
+        querySize = 10;
 
-  var query_splitted = query.trim().split(' ')
-  var final_query = query_splitted.map(x => `${x}~1 `).join('')
+    if (startIndex != 0) {
+        startIndex = querySize * startIndex;
+    }
 
-  const {
-    body
-  } = await client.search({
-    index: elasticIndex,
-    body: {
-      from: startIndex,
-      size: querySize,
-      query: {
-        query_string: {
-          fields: ["text", "title"],
-          query: final_query,
-          default_operator: "AND"
+    var query_splitted = query.trim().split(' ')
+    var final_query = query_splitted.map(x => `${x}~1 `).join('')
+
+    const {
+        body
+    } = await client.search({
+        index: elasticIndex,
+        body: {
+            from: startIndex,
+            size: querySize,
+            query: {
+                query_string: {
+                    fields: ["text", "title"],
+                    query: final_query,
+                    default_operator: "AND"
+                }
+            },
+            highlight: {
+                fields: {
+                    text: {
+                        fragment_size: 150,
+                        number_of_fragments: 3
+                    }
+                },
+                tags_schema: "styled"
+            }
         }
-      },
-      highlight: {
-        fields: {
-          text: {
-            fragment_size: 150,
-            number_of_fragments: 3
-          }
-        },
-        tags_schema: "styled"
-      }
-    }
-  })
+    })
 
-  let results = {};
-  results.hits = body.hits.hits.map(x => {
-    return {
-      title: x._source.title,
-      url: x._source.url,
-      highlight: x.highlight
-    }
-  })
-  results.total = body.hits.total.value;
+    let results = {};
+    results.hits = body.hits.hits.map(x => {
+        return {
+            title: x._source.title,
+            url: x._source.url,
+            highlight: x.highlight
+        }
+    })
+    results.total = body.hits.total.value;
 
-  
 
-  res.send(results)
+
+    res.send(results)
 }));
 
 exports.searchRouter = router;
