6d4b1785d0b6c6c243094796647f531c2d8def4e
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2021-07-01 17:28:54 +0200

    Add test script for docker compose up

---
+++ .github/scripts/docker_compose_up_test.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+set -o errexit # Abort if any command fails
+me=$(basename "$0")
+
+help_message="\
+Launch a Docker Compose and test its output.
+
+Usage:
+  $me [--verbose]
+  $me --help
+
+Arguments:
+  -h, --help                Displays this help screen.
+  -v, --verbose             Increase verbosity. Useful for debugging."
+
+parse_args() {
+    while : ; do
+        if [[ $1 = "-h" || $1 = "--help" ]]; then
+            echo "$help_message"
+            return 0
+        elif [[ $1 = "-v" || $1 = "--verbose" ]]; then
+            verbose=true
+            shift
+        else
+            break
+        fi
+    done
+}
+
+# echo expanded commands as they are executed (for debugging)
+enable_expanded_output() {
+    if [ $verbose ]; then
+        set -o xtrace
+        set +o verbose
+    fi
+}
+
+docker_compose_up_and_test() {
+    url='http://search:3000/?q=payment'
+    { \
+        docker compose up &; \
+        echo $! > .pid; \
+    } | tee /dev/stderr | { \
+        grep -m1 "Listening on 3000" \ # search.developer.swedbankpay.com successfully started
+        && grep -m1 "developer.swedbankpay.com exited with code 0" \ # developer.swedbankpay.com successfully indexed stuff
+        && $("curl --silent http://localhost:3000/?q=payment | grep -m1 '<span class=\"h3 mt-3 search-result-title\">After Payment</span>'")
+        && kill -9 "$(cat .pid)" \
+        && rm .pid; \
+    }
+}
+
+main() {
+    parse_args "$@"
+    enable_expanded_output
+    docker_run_and_test
+}
+
+main "$@"
