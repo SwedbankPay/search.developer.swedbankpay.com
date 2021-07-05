cec981f625b39a930d3eaee32688844c73f8e438
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2021-07-05 12:37:39 +0200

    Run logs in background, fix URL and run curl quietly

--- .github/scripts/docker_compose_up_test.sh
+++ .github/scripts/docker_compose_up_test.sh
@@ -36,20 +36,20 @@ enable_expanded_output() {
 }
 
 docker_compose_up_and_test() {
-    local url='http://search:3000/?q=payment'
+    local url='http://localhost:3000/?q=payment'
     echo 'Starting Docker…'
     docker-compose up --detach
     echo 'Waiting for Developer Portal indexing to complete…'
-    (docker-compose logs --follow developer-portal | \
-        grep -m1 "developer.swedbankpay.com exited with code 0") && \
-        echo 'Developer portal indexing complete!' && \
-        echo 'Waiting for the Search service to become available…' &&
-        docker-compose logs --follow search | \
-        grep -m1 "Listening on 3000" && \
-        echo 'Search service available!' && \
-        echo 'Performing search query…' && \
-        (curl --silent "$url" | grep -m1 '<span class=\"h3 mt-3 search-result-title\">After Payment</span>') && \
-        echo 'Search query completed successfully.'
+    (docker-compose logs --follow developer-portal &) | \
+        grep -m1 -q "developer.swedbankpay.com exited with code 0" && \
+    echo 'Developer portal indexing complete!' && \
+    echo 'Waiting for the Search service to become available…' &&
+    (docker-compose logs --follow search &) | \
+        grep -m1 -q 'Listening on 3000' && \
+    echo 'Search service available!' && \
+    echo 'Performing search query…' && \
+    (curl --silent "$url" | grep -m1 -q '<span class=\"h3 mt-3 search-result-title\">After Payment</span>') && \
+    echo 'Search query completed successfully.'
 
     exit_code=$?
 
