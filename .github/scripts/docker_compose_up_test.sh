e6ddbd2a0ad675bad17ef51f88bd81bda410a3fb
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2021-07-05 10:56:52 +0200

    Simplify conditional execution and piping

--- .github/scripts/docker_compose_up_test.sh
+++ .github/scripts/docker_compose_up_test.sh
@@ -37,17 +37,32 @@ enable_expanded_output() {
 
 docker_compose_up_and_test() {
     local url='http://search:3000/?q=payment'
-    { \
-        docker-compose up & \
-        echo $! > .pid; \
-    } | tee /dev/stderr | { \
-        grep -m1 "Listening on 3000" \
-        && grep -m1 "developer.swedbankpay.com exited with code 0" \
-        && (curl --silent "$url" | grep -m1 '<span class=\"h3 mt-3 search-result-title\">After Payment</span>') \
-        && docker-compose down \
-        && kill -9 "$(cat .pid)" \
-        && rm .pid; \
-    }
+    echo 'Starting Docker…'
+    docker-compose up --detach
+    echo 'Waiting for Developer Portal indexing to complete…'
+    (docker-compose logs --follow developer-portal | \
+        grep -m1 "developer.swedbankpay.com exited with code 0") && \
+        echo 'Developer portal indexing complete!' && \
+        echo 'Waiting for the Search service to become available…' &&
+        docker-compose logs --follow search | \
+        grep -m1 "Listening on 3000" && \
+        echo 'Search service available!' && \
+        echo 'Performing search query…' && \
+        (curl --silent "$url" | grep -m1 '<span class=\"h3 mt-3 search-result-title\">After Payment</span>') && \
+        echo 'Search query completed successfully.'
+
+    exit_code=$?
+
+    if [ $exit_code -eq 0 ]; then
+        echo "Success: The Docker test was completed successfully."
+    else
+        echo "Failure: The Docker test failed." >&2
+    fi
+
+    echo 'Shutting down Docker…'
+    docker-compose down
+
+    return $exit_code
 }
 
 main() {
